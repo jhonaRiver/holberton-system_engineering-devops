ce8496028fa18b327124b1a018d3294d9e1e359a
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-05-31 15:05:17 +0000

    Added file task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+top_ten(subreddit)
+'''
+import requests
+
+
+def top_ten(subreddit):
+    '''
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    '''
+    url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64)\
+               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
+               Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        response = response.json().get('data').get('children')
+        for post in response:
+            title = post.get('data').get('title')
+            print(title)
+    else:
+        print("None")
