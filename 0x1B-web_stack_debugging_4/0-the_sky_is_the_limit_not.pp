0bc3640982a295971b9294974872df9226a70966
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-06-09 15:06:35 +0000

    Added file task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Task 0 Web stack debugging 4
+exec { 'file limit':
+  onlyif    => 'test -e /etc/default/nginx',
+  command   => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  provider  => shell,
+}
